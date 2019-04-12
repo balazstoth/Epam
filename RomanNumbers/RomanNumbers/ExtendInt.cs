3b59a4146cb01df4e91c6cf0e0d92fccd43ebdc2
Balazs_Toth <Balazs_Toth@epam.com>
2019-04-12 09:21:30 +0200

    Roman numbers exercise is done

---
+++ RomanNumbers/RomanNumbers/ExtendInt.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RomanNumbers
+{
+    public static class ExtendInt
+    {
+        private static readonly Dictionary<string, int> keys;
+
+        static ExtendInt()
+        {
+            keys = new Dictionary<string, int>()
+            {
+                { "M", 1000 },
+                { "CM", 900},
+                { "D", 500},
+                { "CD", 400},
+                { "C", 100 },
+                { "XC", 90},
+                { "L", 50},
+                { "XL", 40},
+                { "X", 10 },
+                { "IX", 9 },
+                { "V", 5 },
+                { "IV", 4 },
+                { "I", 1 },
+            };
+        }
+
+        public static string ToRoman(this int number)
+        {
+            if (!isValid(number))
+                return String.Empty;
+
+            string result = String.Empty;
+            foreach (KeyValuePair<string,int> item in keys)
+            {
+                while(number >= item.Value)
+                {
+                    result += item.Key;
+                    number -= item.Value;
+                }
+
+                if (number == 0)
+                    break;
+            }
+
+            return result;
+        }
+
+        private static bool isValid(int i)
+        {
+            return i < 4000 && i > 0;
+        }
+    }
+}
---
+++ RomanNumbers/RomanNumbers/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace RomanNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 3476;
+            Console.WriteLine(n.ToRoman());
+            Console.ReadKey();
+        }
+    }
+}

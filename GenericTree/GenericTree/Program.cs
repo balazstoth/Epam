f709d75ccbd079c287ea683ab1eeabfe4346dac4
Balázs Tóth <tothbalazs@live.com>
2019-04-25 19:35:49 +0200

    Create Tree<T> collection

---
+++ GenericTree/GenericTree/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree = new Tree<int>(1);
+            tree.Root.Add(2);
+            tree.Root.Add(3);
+            tree.Root.Add(4);
+            tree.Root.Add(5);
+
+            Console.WriteLine("Root: " + tree);
+            foreach (int item in tree)
+                Console.WriteLine("Level1: " + item);
+            Console.ReadKey();
+        }
+    }
+}

373f133f65e6f31acbcc79a8cc05034c9271137c
Balázs Tóth <tothbalazs@live.com>
2019-04-25 18:16:42 +0200

    Create GetEvenNumbers

---
+++ GetEvenNumbers/GetEvenNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GetEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (int item in GetEvenNumbers())
+                Console.WriteLine(item);
+
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> GetEvenNumbers()
+        {
+            int i = 0;
+            while (true)
+            {
+                if (i % 2 == 0)
+                    yield return i;
+                i++;
+            }
+        }
+    }
+}
